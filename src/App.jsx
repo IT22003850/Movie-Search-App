d99bc0a0a374742bf0ed65732c71c7723fe04764
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-06-22 14:03:02 +0530

    basic implimented

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useState } from 'react';
+import Search from './components/Search'
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
-    <div>App</div>
+    <main>
+      <div className='pattern'/>
+
+      <div className='wrapper'>
+        <header>
+          <img src='./hero.png' alt='Hero Banner'></img>
+          <h1>Find <span className='text-gradient'>Movies</span>You'll Enjoy Without the Hassle</h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      </div>
+    </main>
   )
 }
 
